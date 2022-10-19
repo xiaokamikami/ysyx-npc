22b8d46a74baabaf14b5228c195bf47b60fabb87
kami <1317379456@qq.com>
2022-10-19 21:42:25 +0800

    添加NEMU时钟模块

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,11 +1,26 @@
 #include <am.h>
 #include <nemu.h>
+#include <stdio.h>
+static uint64_t boot_time = 0;
+
+
+static uint64_t read_time() {
+  //uint64_t lo = *(volatile uint64_t *)(RTC_ADDR );
+  uint32_t lo = *(volatile uint32_t *)(RTC_ADDR );
+  uint32_t hi = *(volatile uint32_t *)(RTC_ADDR+4 );
+  uint64_t time = ((uint64_t)hi << 32) | lo;
+  //printf("get time %ld\n",boot_time);
+  return time ;
+}
 
 void __am_timer_init() {
+  boot_time = read_time();
+  printf("init time:%ld\n",boot_time);
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+  uptime->us =read_time()-boot_time;
+  //printf("us:%d\n",uptime->us);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -166,8 +166,8 @@ static void execute(uint64_t n) {
       g_nr_guest_inst ++;
     }
     else{
-      printf("ERROR:%lx\n",cpu.pc);
-      log_write("---> %s\n",s.logbuf);
+      //printf("ERROR:%lx\n",cpu.pc);
+      //log_write("---> %s\n",s.logbuf);
       return;
     }
 
@@ -209,7 +209,7 @@ void cpu_exec(uint64_t n) {
   switch (nemu_state.state) {
     case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
     case NEMU_END: case NEMU_ABORT:
-      printf("ret:%d\n",nemu_state.halt_ret);
+      //printf("ret:%d\n",nemu_state.halt_ret);
       Log("nemu: %s at pc = " FMT_WORD,
           (nemu_state.state == NEMU_ABORT ? ANSI_FMT("ABORT", ANSI_FG_RED) :
            (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
