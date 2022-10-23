827a94f9bff14277eb82946bdb4493e1a17cb3b7
kami <1317379456@qq.com>
2022-10-23 22:43:56 +0800

    完成键盘

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -1,9 +1,16 @@
 #include <am.h>
 #include <nemu.h>
-
+#include <amdev.h>
+#include <stdio.h>
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  int k = *(volatile uint32_t *)(KBD_ADDR);
+  kbd->keydown = (k & KEYDOWN_MASK ? true : false);
+  if(kbd->keydown==true){
+    kbd->keycode = k & ~KEYDOWN_MASK;
+    //printf("k=%ld\n",k);  // down£ºcode
+    printf("keycode = %d\n",kbd->keycode);
+  }
+  else kbd->keycode = AM_KEY_NONE;
 }
