c765a1d7062320977d61750b522669c05fc10a21
22041412 <1317379456@qq.com>
2023-08-26 00:26:48 +0800

    写好Icache命中情况的处理

--- npc/vsrc/Icache.v
+++ npc/vsrc/Icache.v
@@ -1,6 +1,5 @@
-// 16组相连cache  
+// 16路相连cache  每路64行
 // cache 大小为16kb  能容纳 1024个块  块大小为16B
-// 每组64 line 
 // index位宽= 2log(64) =  6
 // 每个cache块都可放到组号为(tag % 组数)中的任意行
 module ysyx_22041412_Icache(
@@ -8,23 +7,29 @@ module ysyx_22041412_Icache(
     input                   rst,
 //cpu       <---> icache
     input       [31:0]      cpu_req_addr,
-    input                   cpu_req_valid,
-    output  reg [31:0]      cpu_data_read,
+    input                   cpu_valid,
+    output      [31:0]      cpu_read_data,
     output  reg             cpu_ready,
 //icache    <---> AXI
-
+    input  					axi_ready_i,        // 读有效等待接收
+    output 					axi_valid,          // 发出读请求
+    output  reg 			axi_ready_o,         
+    output  reg [7:0] 		axi_r_size_i,
+    input       [63:0]		axi_r_data_i, 		// 读数据
+    output  reg [31:0]		axi_r_addr_o  		// 读地址
 );
     parameter ram_widh         = 'd128
     parameter cache_size       = 'd1024;
-    parameter cache_line_size  = 'd64;
+    parameter cache_line_size  = 'd128;
     parameter cache_bank_size  = 'd16;
-    parameter cache_pape_widh  = 'd16;
+    parameter cache_pape_widh  = 'd8;
     parameter cpu_size         = 'd4;
 
-`define IDLE         2'b00  
-`define INST         2'b01  
-`define DATA         2'b10  
-`define READY        2'b11 
+`define IDLE         3'b000  
+`define INST         3'b001  
+`define RD_RAM       3'b010  
+`define RD_CACHE     3'b011  
+`define READY        3'b100 
 /* 
     31     11 9    4 3      0                   127       0
    +---------+-------+--------+                 +---------+
@@ -35,28 +40,43 @@ wire [21:0] cache_tag
 wire [5:0]  cache_index;
 wire [3:0]  cache_offset;
 
-assign cache_tag    = cpu_req_addr{31:10};
+wire  ram_rd_data [`ram_widh-1:0] ;        //CACHE读数据
+wire  ram_rd_cen   ; 				   	   //CACHE读使能
+
+
+assign cache_tag    = cpu_req_addr{31:11};
+assign ram_rd_cen   = cpu_req_addr[10];
 assign cache_index  = cpu_req_addr{9:4};
 assign cache_offset = cpu_req_addr{3:0};
 
-// 1'b1v + tag 
-reg [21+1'b1:0] cache_tag_ram [`cache_line_size-1:0] [0:`cache_pape_widh-1]; //tag堆
+// tag 
+reg [21:0] cache_tag_ram [6:0] [4:0]; //tag堆
 
 
-wire ram_rd_data [`ram_widh-1:0] ;        //读数据
-wire ram_rd_cen  [`cache_pape_widh-1:0] ; //读使能
-
-wire [3:0]ram_len = cache_tag % 16 ;
 
 
+reg [127:0] read_data;
+
+assign cpu_read_data = (cache_offset=='b0000)?read_data[31:0] :
+					   (cache_offset=='b0100)?read_data[63:32] :
+					   (cache_offset=='d1000)?read_data[95:64] :
+					   (cache_offset=='d1100)?read_data[127:96] ;
 genvar index; //生成存储器
 generate
-    for(index=0; index<16; index=index+1) //例化16个1k ram模块
+    for(index=0; index<8; index=index+1) //例化16个1k ram模块
     begin: paper
-        ysyx_22041412_S011HD1P_X32Y2D128 cache_ram(
+        ysyx_22041412_S011HD1P_X32Y2D128 cache_ram1(
             CLK (clk),
-            Q   (ram_rd_data), 
-            CEN (ram_rd_cen[index]), 
+            Q   (ram_rd_data[index]), 
+            CEN (ram_rd_cen[0]), 
+            WEN (1'b1), 
+            A   (cache_index), 
+            D   (128'b0)
+        );
+		ysyx_22041412_S011HD1P_X32Y2D128 cache_ram2(
+            CLK (clk),
+            Q   (ram_rd_data[index]), 
+            CEN (ram_rd_cen[1]), 
             WEN (1'b1), 
             A   (cache_index), 
             D   (128'b0)
@@ -66,8 +86,13 @@ endgenerate
 
 
 
-reg[2:0] switch;              //cache状态机 
-reg[2:0] next_switch;
+reg [6:0]ram_len;  //本次访问的行
+reg hit;		   //命中标识
+reg [5:0]tag_v;	   //命中位置
+reg[3:0] switch;              //cache状态机 
+reg[3:0] next_switch;
+
+
 
     always@(posedge clk)begin //状态机更新
       if(rst )begin
@@ -84,35 +109,60 @@ reg[2:0] next_switch;
       end else begin
         case(state) //写入状态机的控制 
         `IDLE: begin
-            if(cpu_req_valid) next_state = `INST;
-            else next_state = `IDLE;
+            if(cpu_valid) 
+            	next_state = `INST;
+            else 
+            	next_state = `IDLE;
         end
         `INST:begin
-            
+			if(hit) 
+				next_state = `READY;
+			else 
+				next_state = `RD_RAM;
         end
-        `DATA:begin
-        end
+        `RD_RAM:begin
+
+		end
         `READY:begin
+			next_state = `IDLE;  
         end
         default: next_state = `IDLE;
         endcase
       end
     end
 
-    integer i;
+
     always@(posedge clk)begin //cache执行状态机
       if(rst )begin
 
       end else begin
         case(state) 
         `IDLE: begin
+			cpu_ready <= 1'b0;
+			ram_len   <= cache_tag % 'd64;
         end
-        `INST:begin
-            if(cache_tag_ram[i][ram_len])==cache_tag;
+        `INST:begin  //检查相关位置的TAG是否命中
+			if(hit)begin
+				case (tag_v):
+					'd0: read_data = ram_rd_data[0];
+					'd1: read_data = ram_rd_data[1];
+					'd2: read_data = ram_rd_data[2];
+					'd3: read_data = ram_rd_data[3];
+					'd4: read_data = ram_rd_data[4];
+					'd5: read_data = ram_rd_data[5];
+					'd6: read_data = ram_rd_data[6];
+					'd7: read_data = ram_rd_data[7];
+					default:begin
+						read_data = 'd0;
+					end
+				endcase
+			end 
         end
-        `DATA:begin
-        end
+        `RD_RAM:begin	//没有命中，从AXI请求内存
+
+		end
         `READY:begin
+			cpu_ready <= 1'b1;
         end
         default: ;
         endcase
@@ -120,4 +170,24 @@ reg[2:0] next_switch;
     end
 
 
+
+always (*)begin
+    if(cache_tag_ram[ram_len][0]==cache_tag ) begin
+				tag_v     = 'd0;
+	end else if(cache_tag_ram[ram_len][1]==cache_tag ) begin
+				tag_v     = 'd1;
+	end else if(cache_tag_ram[ram_len][2]==cache_tag ) begin
+				tag_v     = 'd2;
+	end else if(cache_tag_ram[ram_len][3]==cache_tag ) begin
+				tag_v     = 'd3;
+	end else if(cache_tag_ram[ram_len][4]==cache_tag ) begin
+				tag_v     = 'd4;
+	end else if(cache_tag_ram[ram_len][5]==cache_tag ) begin
+				tag_v     = 'd5;
+	end else if(cache_tag_ram[ram_len][6]==cache_tag ) begin
+				tag_v     = 'd6;
+	end else if(cache_tag_ram[ram_len][7]==cache_tag ) begin
+				tag_v     = 'd7;
+	end else 	hit       = 1'b0;
+end
 endmodule
