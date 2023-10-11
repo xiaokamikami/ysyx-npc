98f45234b2b0d024d5c97c8dd6ac77aed3345def
22041412 <1317379456@qq.com>
2023-10-11 16:02:30 +0800

    清楚git缓存，删了错误的备份

---
+++ nemu/tools/spike-diff/repo/softfloat/dummy_for_pa.c
@@ -0,0 +1,11 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include "platform.h"
+#include "internals.h"
+#include "specialize.h"
+#include "softfloat.h"
+
+float32_t f16_to_f32( float16_t a ) {
+  float32_t x = {};
+  return x;
+}
